39062eb374f4d2cec8b15c9725b571f121421aab
Lewis Lockhart <lewis00001@gmail.com>
2019-11-10 20:27:48 -0700

    added maya obj, images, start files

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    // test image to remove later
+    $("#test").html("<img src='assets/images/glyph/" + maya.glyph[21].img + "'>");
+
+
+    // *************************************** //
+    // global var setup
+    // *************************************** //
+
+
+});
---
+++ assets/js/maya.js
@@ -0,0 +1,150 @@
+// information found on http://research.famsi.org/montgomery_dictionary/mt_search.php
+
+let maya = {
+        glyph: [
+            {
+                img: "0.gif",
+                pro: "NIKTE'-te'",
+                // ans[0] is the correct answer
+                ans: ["flower", "ocean", "cat", "sunset"]
+            },
+            {
+                img: "1.gif",
+                pro: "a-AK'OT-ta",
+                // ans[0] is the correct answer
+                ans: ["dance", "turtle", "spring", "warrior"]
+            },
+            {
+                img: "2.gif",
+                pro: "AJAW-le-le",
+                // ans[0] is the correct answer
+                ans: ["kingship", "obtain", "supply", "earth"]
+            },
+            {
+                img: "3.gif",
+                pro: "ajaw",
+                // ans[0] is the correct answer
+                ans: ["lord", "warrior", "family", "west"]
+            },
+            {
+                img: "4.gif",
+                pro: "b'utz",
+                // ans[0] is the correct answer
+                ans: ["smoke", "rice", "spring", "river"]
+            },
+            {
+                img: "5.gif",
+                pro: "b'i/b'ih",
+                // ans[0] is the correct answer
+                ans: ["road", "war", "pig", "tribe"]
+            },
+            {
+                img: "6.gif",
+                pro: "b'alam",
+                // ans[0] is the correct answer
+                ans: ["jaguar", "north", "lion", "summer"]
+            },
+            {
+                img: "7.gif",
+                pro: "b'ak",
+                // ans[0] is the correct answer
+                ans: ["bone", "sleep", "death", "sight"]
+            },
+            {
+                img: "8.gif",
+                pro: "chan/kaan",
+                // ans[0] is the correct answer
+                ans: ["sky", "death", "wisdom", "ruler"]
+            },
+            {
+                img: "9.gif",
+                pro: "chan/kan",
+                // ans[0] is the correct answer
+                ans: ["captor", "progress", "sorrow", "harvest"]
+            },
+            {
+                img: "10.gif",
+                pro: "Ch'ul",
+                // ans[0] is the correct answer
+                ans: ["sacred", "family", "slave", "year"]
+            },
+            {
+                img: "11.gif",
+                pro: "ch'ok",
+                // ans[0] is the correct answer
+                ans: ["young", "woman", "slave", "reunion"]
+            },
+            {
+                img: "12.gif",
+                pro: "ch'en",
+                // ans[0] is the correct answer
+                ans: ["cave", "food", "daughter", "mother"]
+            },
+            {
+                img: "13.gif",
+                pro: "ch'am",
+                // ans[0] is the correct answer
+                ans: ["harvest", "choke", "sheep", "night"]
+            },
+            {
+                img: "14.gif",
+                pro: "ch'ab'",
+                // ans[0] is the correct answer
+                ans: ["sacrifice", "omen", "remember", "birth"]
+            },
+            {
+                img: "15.gif",
+                pro: "ch'at",
+                // ans[0] is the correct answer
+                ans: ["dwarf", "spring", "ancestor", "enemy"]
+            },
+            {
+                img: "16.gif",
+                pro: "eb'",
+                // ans[0] is the correct answer
+                ans: ["stairway", "waterfall", "remember", "breath"]
+            },
+            {
+                img: "17.gif",
+                pro: "juliy",
+                // ans[0] is the correct answer
+                ans: ["arrived", "thunder", "quest", "sought"]
+            },
+            {
+                img: "18.gif",
+                pro: "jom",
+                // ans[0] is the correct answer
+                ans: ["destroy", "son", "home", "artist"]
+            },
+            {
+                img: "19.gif",
+                pro: "ja'ab'",
+                // ans[0] is the correct answer
+                ans: ["yeah", "sky", "road", "peace"]
+            },
+            {
+                img: "20.gif",
+                pro: "ix/ixik",
+                // ans[0] is the correct answer
+                ans: ["woman", "builder", "climb", "creation"]
+            },
+            {
+                img: "21.gif",
+                pro: "itz'at",
+                // ans[0] is the correct answer
+                ans: ["wiseman", "hunter", "heavens", "mountain"]
+            },
+            {
+                img: "22.gif",
+                pro: "ikatz",
+                // ans[0] is the correct answer
+                ans: ["burden", "rest", "found", "prophecy"]
+            },
+            {
+                img: "23.gif",
+                pro: "Tnn",
+                // ans[0] is the correct answer
+                ans: ["ownership", "weakness", "spring", "enemy"]
+            }
+        ]
+    }
