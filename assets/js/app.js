8db3b171fe28ae9ad262c45e5e02532816866045
Lewis Lockhart <lewis00001@gmail.com>
2019-11-11 18:54:02 -0700

    added html and js for question generation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,47 @@
 $(document).ready(function () {
 
-    // test image to remove later
-    $("#test").html("<img src='assets/images/glyph/" + maya.glyph[21].img + "'>");
-
-
     // *************************************** //
     // global var setup
     // *************************************** //
+    // stores selected glyphs so the same rendom one is not selected twice
+    let selectedGlyphs = [];
 
 
     // ************************************** //
     // instructions / start game - (screen 1)
     // ************************************** //
-    
+    $(".startButton").on("click", function () {
+        // play sound
+        $("#tap")[0].play();
+        // add / remove classes
+        $(".screen_1").addClass("hidden");
+        $(".screen_2").removeClass("hidden");
+        // load screen 2 question
+        selectGlyph();
+    });
 
     // ************************************** //
     //  - (screen 2)
     // ************************************** //
+    function displayQuestion(img, pro, ans) {
+        $(".displayQ").html("<div><img class='glyphImg' src='assets/images/glyph/" + img + "'></div><br>" + 
+                            "<div>" + pro + "</div><br>" + 
+                            "<div>" + ans + "</div>");
+    }
+
+    function selectGlyph() {
+        while (selectedGlyphs.length < 10) {
+            let glyphNum = maya.glyph[Math.floor(Math.random() * maya.glyph.length)];
+            if ($.inArray(glyphNum.oid, selectedGlyphs) === -1) {
+                selectedGlyphs.push(glyphNum.oid);
+                console.log(glyphNum.oid);
+                console.log(selectedGlyphs);
+            }
+            displayQuestion(glyphNum.img, glyphNum.pro, glyphNum.ans);
+        }
+    }
+    
+
 
 
     // ************************************** //
--- assets/js/maya.js
+++ assets/js/maya.js
@@ -3,144 +3,168 @@
 let maya = {
         glyph: [
             {
+                oid: "g000",
                 img: "0.gif",
                 pro: "NIKTE'-te'",
                 // ans[0] is the correct answer
                 ans: ["flower", "ocean", "cat", "sunset"]
             },
             {
+                oid: "g001",
                 img: "1.gif",
                 pro: "a-AK'OT-ta",
                 // ans[0] is the correct answer
                 ans: ["dance", "turtle", "spring", "warrior"]
             },
             {
+                oid: "g002",
                 img: "2.gif",
                 pro: "AJAW-le-le",
                 // ans[0] is the correct answer
                 ans: ["kingship", "obtain", "supply", "earth"]
             },
             {
+                oid: "g003",
                 img: "3.gif",
                 pro: "ajaw",
                 // ans[0] is the correct answer
                 ans: ["lord", "warrior", "family", "west"]
             },
             {
+                oid: "g004",
                 img: "4.gif",
                 pro: "b'utz",
                 // ans[0] is the correct answer
                 ans: ["smoke", "rice", "spring", "river"]
             },
             {
+                oid: "g005",
                 img: "5.gif",
                 pro: "b'i/b'ih",
                 // ans[0] is the correct answer
                 ans: ["road", "war", "pig", "tribe"]
             },
             {
+                oid: "g006",
                 img: "6.gif",
                 pro: "b'alam",
                 // ans[0] is the correct answer
                 ans: ["jaguar", "north", "lion", "summer"]
             },
             {
+                oid: "g007",
                 img: "7.gif",
                 pro: "b'ak",
                 // ans[0] is the correct answer
                 ans: ["bone", "sleep", "death", "sight"]
             },
             {
+                oid: "g008",
                 img: "8.gif",
                 pro: "chan/kaan",
                 // ans[0] is the correct answer
                 ans: ["sky", "death", "wisdom", "ruler"]
             },
             {
+                oid: "g009",
                 img: "9.gif",
                 pro: "chan/kan",
                 // ans[0] is the correct answer
                 ans: ["captor", "progress", "sorrow", "harvest"]
             },
             {
+                oid: "g010",
                 img: "10.gif",
                 pro: "Ch'ul",
                 // ans[0] is the correct answer
                 ans: ["sacred", "family", "slave", "year"]
             },
             {
+                oid: "g011",
                 img: "11.gif",
                 pro: "ch'ok",
                 // ans[0] is the correct answer
                 ans: ["young", "woman", "slave", "reunion"]
             },
             {
+                oid: "g012",
                 img: "12.gif",
                 pro: "ch'en",
                 // ans[0] is the correct answer
                 ans: ["cave", "food", "daughter", "mother"]
             },
             {
+                oid: "g013",
                 img: "13.gif",
                 pro: "ch'am",
                 // ans[0] is the correct answer
                 ans: ["harvest", "choke", "sheep", "night"]
             },
             {
+                oid: "g014",
                 img: "14.gif",
                 pro: "ch'ab'",
                 // ans[0] is the correct answer
                 ans: ["sacrifice", "omen", "remember", "birth"]
             },
             {
+                oid: "g015",
                 img: "15.gif",
                 pro: "ch'at",
                 // ans[0] is the correct answer
                 ans: ["dwarf", "spring", "ancestor", "enemy"]
             },
             {
+                oid: "g016",
                 img: "16.gif",
                 pro: "eb'",
                 // ans[0] is the correct answer
                 ans: ["stairway", "waterfall", "remember", "breath"]
             },
             {
+                oid: "g017",
                 img: "17.gif",
                 pro: "juliy",
                 // ans[0] is the correct answer
                 ans: ["arrived", "thunder", "quest", "sought"]
             },
             {
+                oid: "g018",
                 img: "18.gif",
                 pro: "jom",
                 // ans[0] is the correct answer
                 ans: ["destroy", "son", "home", "artist"]
             },
             {
+                oid: "g019",
                 img: "19.gif",
                 pro: "ja'ab'",
                 // ans[0] is the correct answer
                 ans: ["yeah", "sky", "road", "peace"]
             },
             {
+                oid: "g020",
                 img: "20.gif",
                 pro: "ix/ixik",
                 // ans[0] is the correct answer
                 ans: ["woman", "builder", "climb", "creation"]
             },
             {
+                oid: "g021",
                 img: "21.gif",
                 pro: "itz'at",
                 // ans[0] is the correct answer
                 ans: ["wiseman", "hunter", "heavens", "mountain"]
             },
             {
+                oid: "g022",
                 img: "22.gif",
                 pro: "ikatz",
                 // ans[0] is the correct answer
                 ans: ["burden", "rest", "found", "prophecy"]
             },
             {
+                oid: "g023",
                 img: "23.gif",
                 pro: "Tnn",
                 // ans[0] is the correct answer
