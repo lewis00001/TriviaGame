1b8b5fedad854900e7163884cdbe6820b3380d14
Lewis Lockhart <lewis00001@gmail.com>
2019-11-13 11:27:48 -0700

    displayed answers now random - logic added to know when correct ans is clicked

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,29 +24,51 @@ $(document).ready(function () {
     //  - (screen 2)
     // ************************************** //
     function displayQuestion(img, pro, ans) {
+        // randomized order of output
+        let randOutput = [];
+        while (randOutput.length < 4) {
+            let current = ans[Math.floor(Math.random() * 4)];
+            if ($.inArray(current.a, randOutput) === -1) {
+                randOutput.push(current.a);
+            }
+        }
+        console.log(randOutput);
+        console.log(selectedGlyphs);
+        // output to screen
         $(".displayQ").html("<div class='imgBox'><img class='glyphImg' src='assets/images/glyph/" + 
                             img + "'></div>" + 
                             "<div class='textCenter'><span id='pro'>pron: (" + pro + ")</span></div><br>" + 
                             "<div class='flexbox'>" + 
-                            "<div class='optionButton'>" + ans[0].a + "</div>" +
-                            "<div class='optionButton'>" + ans[1].a + "</div>" +
-                            "<div class='optionButton'>" + ans[2].a + "</div>" +
-                            "<div class='optionButton'>" + ans[3].a + "</div>" +
+                            "<div class='optionButton'>" + randOutput[0] + "</div>" +
+                            "<div class='optionButton'>" + randOutput[1] + "</div>" +
+                            "<div class='optionButton'>" + randOutput[2] + "</div>" +
+                            "<div class='optionButton'>" + randOutput[3] + "</div>" +
                             "</div>"
                             );
     }
 
     function selectGlyph() {
-        while (selectedGlyphs.length < 10) {
             let glyphNum = maya.glyph[Math.floor(Math.random() * maya.glyph.length)];
             if ($.inArray(glyphNum.oid, selectedGlyphs) === -1) {
                 selectedGlyphs.push(glyphNum.oid);
-                console.log(glyphNum.oid);
-                console.log(selectedGlyphs);
+                //console.log(glyphNum.oid);
+                //console.log(selectedGlyphs);
             }
             displayQuestion(glyphNum.img, glyphNum.pro, glyphNum.ans);
-        }
+            // listen for option click
+            $(".optionButton").click(function(event) {
+                let theChosen = $(this).html();
+                console.log(theChosen);
+                // eval of ans - true/false
+                if (theChosen === glyphNum.ans[0].a) {
+                  console.log(glyphNum.ans[0].isTrue);
+                } else {
+                    console.log("not a match");
+                }
+            });
     }
+
+
     
 
 
