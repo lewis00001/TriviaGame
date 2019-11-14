e51059a8a3134599f5df160167fe11fbd299bc8d
Lewis Lockhart <lewis00001@gmail.com>
2019-11-13 18:23:16 -0700

    added css and js for countdown timer - displays on page

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,41 +35,66 @@ $(document).ready(function () {
         console.log(randOutput);
         console.log(selectedGlyphs);
         // output to screen
-        $(".displayQ").html("<div class='imgBox'><img class='glyphImg' src='assets/images/glyph/" + 
-                            img + "'></div>" + 
-                            "<div class='textCenter'><span id='pro'>pron: (" + pro + ")</span></div><br>" + 
-                            "<div class='flexbox'>" + 
-                            "<div class='optionButton'>" + randOutput[0] + "</div>" +
-                            "<div class='optionButton'>" + randOutput[1] + "</div>" +
-                            "<div class='optionButton'>" + randOutput[2] + "</div>" +
-                            "<div class='optionButton'>" + randOutput[3] + "</div>" +
-                            "</div>"
-                            );
+        $(".displayQ").html("<div class='imgBox'><img class='glyphImg' src='assets/images/glyph/" +
+            img + "'></div>" +
+            "<div class='textCenter'><span id='pro'>pron: (" + pro + ")</span></div>" +
+            "<div id='timerBox'><div id='timerBar'></div></div><br>" +
+            "<div class='flexbox'>" +
+            "<div class='optionButton'>" + randOutput[0] + "</div>" +
+            "<div class='optionButton'>" + randOutput[1] + "</div>" +
+            "<div class='optionButton'>" + randOutput[2] + "</div>" +
+            "<div class='optionButton'>" + randOutput[3] + "</div>" +
+            "</div>"
+        );
+
+        // calls questionTimer
+        questionTimer();
     }
 
     function selectGlyph() {
-            let glyphNum = maya.glyph[Math.floor(Math.random() * maya.glyph.length)];
-            if ($.inArray(glyphNum.oid, selectedGlyphs) === -1) {
-                selectedGlyphs.push(glyphNum.oid);
-                //console.log(glyphNum.oid);
-                //console.log(selectedGlyphs);
+        // selects a random glyph object
+        let glyphNum = maya.glyph[Math.floor(Math.random() * maya.glyph.length)];
+        // ensure glyph obj has not already been used
+        if ($.inArray(glyphNum.oid, selectedGlyphs) === -1) {
+            selectedGlyphs.push(glyphNum.oid);
+        }
+
+        // calls displayQuestion()
+        displayQuestion(glyphNum.img, glyphNum.pro, glyphNum.ans);
+
+        // listen for option click
+        $(".optionButton").click(function (event) {
+            let theChosen = $(this).html();
+            console.log(theChosen);
+            // eval of ans - true/false
+            if (theChosen === glyphNum.ans[0].a) {
+                console.log(glyphNum.ans[0].isTrue);
+            } else {
+                console.log("not a match");
             }
-            displayQuestion(glyphNum.img, glyphNum.pro, glyphNum.ans);
-            // listen for option click
-            $(".optionButton").click(function(event) {
-                let theChosen = $(this).html();
-                console.log(theChosen);
-                // eval of ans - true/false
-                if (theChosen === glyphNum.ans[0].a) {
-                  console.log(glyphNum.ans[0].isTrue);
-                } else {
-                    console.log("not a match");
-                }
-            });
+        });
+    }
+
+    // questionTimer display
+    function questionTimer() {
+        let width = 101;
+        let durration = 15; // time in seconds
+        setInterval(update, durration * 10);
+        // make updates
+        function update() {
+            if (width >= 0) {
+                $("#timerBar").css({
+                    "width": width + "%"
+                });
+                console.log(width);
+                width--;
+            } else {
+                return false;
+            }
+        };
     }
 
 
-    
 
 
 
